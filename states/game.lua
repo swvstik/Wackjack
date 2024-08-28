fc5b645bcecfe1cae0db8a606c40d4c9ec77abdb
Swastik <120981863+swvstik@users.noreply.github.com>
2024-08-28 15:56:33 +0530

    modified:   states/game.lua

--- states/game.lua
+++ states/game.lua
@@ -1,4 +1,28 @@
+math.randomseed(os.time())
 local game = {}
+
+local rank = {"ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king"}
+local suits = {"hearts", "diamonds", "clubs", "spades"}
+
+local cardset = {}
+for i = 1, #rank do
+    for j = 1, #suits do
+        local mid=rank[i] .. "_of_" .. suits[j]
+        table.insert(cardset,mid)
+    end
+end
+
+sel = nil
+
+
+-- function pickrandom(cardset)
+--     local rindex = math.random(#cardset)
+--     return cardset[rindex]
+-- end
+
+for i=1, #cardset do
+    print(cardset[i])
+end
 
 function game:load()
 
@@ -8,8 +32,9 @@ function game:update(dt)
 
 end
 
-function game:draw(dt)
-
+function game:draw()
+    love.graphics.draw(love.graphics.newImage('assets/cards/' .. sel .. '.png'),0,0)
+    print(sel)
 end
 
 function game:keypressed(key)
